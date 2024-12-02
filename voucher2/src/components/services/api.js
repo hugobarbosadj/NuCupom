f5e1d6a825514308460997d99ceed75b279acd64
Hugo Barbosa <hugobarbosadj@hotmail.com>
2024-12-02 00:13:45 -0300

    1.2.2

--- voucher2/src/components/services/api.js
+++ voucher2/src/components/services/api.js
@@ -1,6 +1,41 @@
 // api.js
 import axios from 'axios';
 
+// Configuração inicial do Axios
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8080/', // URL base para o servidor
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// Interceptador para adicionar o token de autenticação a todas as requisições
+// Interceptador para adicionar o token de autenticação a todas as requisições (exceto para alguns endpoints)
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token'); // Recupera o token do localStorage
+
+        // Excluir o cabeçalho Authorization em endpoints específicos (como o de busca de CEP)
+        if (!config.url.includes('/cep/buscar') && token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+
+// Interceptador para tratamento de respostas ou erros globais
+axiosInstance.interceptors.response.use(
+    (response) => response.data, // Retorna os dados da resposta diretamente
+    (error) => {
+        const errorResponse = error.response || {};
+        const errorMessage =
+            errorResponse.data?.message || `Erro: ${errorResponse.status} - ${errorResponse.statusText}`;
+        return Promise.reject(new Error(errorMessage));
+    }
+);
+
 
 // Criação da instância axios
 const api = axios.create({
